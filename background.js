9c97fce96d713f1233294a9bb39922f0f89f31b0
jamsteh <54650706+jamsteh@users.noreply.github.com>
2019-09-07 13:01:38 -0500

    Update background.js

--- background.js
+++ background.js
@@ -3,10 +3,23 @@
 // found in the LICENSE file.
 
 // Called when the user clicks on the browser action.
+chrome.runtime.onInstalled.addListener(function() {
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   console.log('Turning ' + tab.url + ' red!');
   chrome.tabs.executeScript({
     code: 'document.body.style.backgroundColor="red"'
   });
+});
+
+// Called when user visits the Howdy schedule.
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'compassxe-ssb.tamu.edu'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
 });
